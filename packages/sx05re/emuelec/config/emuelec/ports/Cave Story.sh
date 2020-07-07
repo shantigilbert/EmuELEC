d55e4c8de26baaea4390d3c16348ffe20891bc66
Sylvia van Os <sylvia@hackerchick.me>
2020-07-07 19:31:08 +0200

    Add sanity checks when launching Cave Story

--- packages/sx05re/emuelec/config/emuelec/ports/Cave Story.sh
+++ packages/sx05re/emuelec/config/emuelec/ports/Cave Story.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+if [[ ! -f /storage/roms/ports/CaveStory/Doukutsu.exe ]]; then
+    /emuelec/scripts/fbterm.sh "echo Could not find Doukutsu.exe, please make sure you copied the game into /storage/roms/ports/CaveStory.; sleep 10"
+    exit 1
+fi
+
+if [[ ! -d /storage/roms/ports/CaveStory/data ]]; then
+    /emuelec/scripts/fbterm.sh "echo Could not find the data directory, please make sure you copied the game data into /storage/roms/ports/CaveStory/data.; sleep 10"
+    exit 1
+fi
+
 /emuelec/scripts/emuelecRunEmu.sh "/storage/roms/ports/CaveStory/Doukutsu.exe" -Pports "${2}" -Cnxengine "-SC${0}"
