3394411a656d3ae7575ed8a1c69ad1cbdb76db73
Sylvia van Os <sylvia@hackerchick.me>
2020-09-11 18:47:03 +0200

    Ignore SETUP.EXE, INSTALL.EXE and INSTALLER.EXE

--- packages/sx05re/emulators/dosbox-sdl2/config/games/+ Scan DOSBox Games.sh
+++ packages/sx05re/emulators/dosbox-sdl2/config/games/+ Scan DOSBox Games.sh
@@ -12,36 +12,42 @@ rp_registerAllModules
 joy2keyStart
 
 function restart_confirm() {
-     if dialog --ascii-lines --yesno "DOSBox scan completed, any found games will appear next time you restart Emulationstation, do you want to restart it now?"  22 76 >/dev/tty; then
-		systemctl restart emustation
-      fi
+    if dialog --ascii-lines --yesno "DOSBox scan completed, any found games will appear next time you restart Emulationstation, do you want to restart it now?"  22 76 >/dev/tty; then
+        systemctl restart emustation
+    fi
 
-	if [ "$EE_DEVICE" == "OdroidGoAdvance" ]; then
-		killall kmscon
-	fi
+    if [ "$EE_DEVICE" == "OdroidGoAdvance" ]; then
+        killall kmscon
+    fi
 }
 
 function create_launcher() {
-      launcher_name="$1 ($2)"
+    launcher_name="$1 ($2)"
 
-      dos_dirname="${1:0:6}~1"
+    dos_dirname="${1:0:6}~1"
 
-      mkdir -p "/storage/roms/pc/$launcher_name"
-      cp /storage/.config/dosbox/dosbox-SDL2.conf "/storage/roms/pc/$launcher_name"
-      echo "mount c /storage/roms/pcdata" >> "/storage/roms/pc/$launcher_name/dosbox-SDL2.conf"
-      echo "c:" >> "/storage/roms/pc/$launcher_name/dosbox-SDL2.conf"
-      echo "cd $dos_dirname" >> "/storage/roms/pc/$launcher_name/dosbox-SDL2.conf"
-      echo "$2" >> "/storage/roms/pc/$launcher_name/dosbox-SDL2.conf"
-      echo "exit" >> "/storage/roms/pc/$launcher_name/dosbox-SDL2.conf"
+    mkdir -p "/storage/roms/pc/$launcher_name"
+    cp /storage/.config/dosbox/dosbox-SDL2.conf "/storage/roms/pc/$launcher_name"
+    echo "mount c /storage/roms/pcdata" >> "/storage/roms/pc/$launcher_name/dosbox-SDL2.conf"
+    echo "c:" >> "/storage/roms/pc/$launcher_name/dosbox-SDL2.conf"
+    echo "cd $dos_dirname" >> "/storage/roms/pc/$launcher_name/dosbox-SDL2.conf"
+    echo "$2" >> "/storage/roms/pc/$launcher_name/dosbox-SDL2.conf"
+    echo "exit" >> "/storage/roms/pc/$launcher_name/dosbox-SDL2.conf"
 
-      touch "/storage/roms/pc/$launcher_name/$launcher_name.bat"
+    touch "/storage/roms/pc/$launcher_name/$launcher_name.bat"
 }
 
 for data_dir in /storage/roms/pcdata/*; do
-      if [ -d "$data_dir" ]; then
-	    for executable in $(find "$data_dir" -iname "*.exe"); do
-		  create_launcher "$(basename $data_dir)" "$(basename $executable)"
-	    done
-      fi
+    if [ -d "$data_dir" ]; then
+        for executable in $(find "$data_dir" -iname "*.exe"); do
+            case "$executable" in
+                "SETUP.EXE" | "INSTALL.EXE" | "INSTALLER.EXE")
+                    ;;
+                *)
+                    create_launcher "$(basename $data_dir)" "$(basename $executable)"
+                    ;;
+            esac
+        done
+    fi
 done
 restart_confirm
