1b166bd1f67282da24d275b4619dd9307211e5da
joslan <Langerz82>
2021-08-13 01:13:45 +1000

    whitespace cleanup

--- packages/sx05re/emuelec/bin/emuelecRunEmu.sh
+++ packages/sx05re/emuelec/bin/emuelecRunEmu.sh
@@ -81,6 +81,11 @@ CORE="${CORE%% *}"  # until a space is found
 EMULATOR="${arguments##*--emulator=}"  # read from --emulator= onwards
 EMULATOR="${EMULATOR%% *}"  # until a space is found
 
+SET_DISPLAY_SH="/usr/bin/setres.sh" # source of set-display script.
+VIDEO=$(get_ee_setting global.videomode)
+[[ -z "$VIDEO" ]] && VIDEO=$(get_ee_setting ee_videomode)
+VIDEO_EMU=$(get_ee_setting ${PLATFORM}.nativevideo)
+
 ROMNAME="$1"
 BASEROMNAME=${ROMNAME##*/}
 GAMEFOLDER="${ROMNAME//${BASEROMNAME}}"
@@ -148,6 +153,8 @@ fi
 SPL=$(get_ee_setting ee_splash.enabled)
 [ "$SPL" -eq "1" ] && ${TBASH} show_splash.sh "$PLATFORM" "${ROMNAME}"
 
+# Set the display video to that of the emulator setting.
+[[ -z "$VIDEO_EMU" ]] && source $SET_DISPLAY_SH $VIDEO_EMU # set display
 
 if [ -z ${LIBRETRO} ] && [ -z ${RETRORUN} ]; then
 
@@ -421,6 +428,9 @@ fi
 
 # Only run fbfix on Amlogic-ng (Mali g31 and g52 in Amlogic SOC)
 [[ "$EE_DEVICE" == "Amlogic-ng" ]] && fbfix
+
+# Revert the display video to that of the original emuelec setting.
+[[ -z "$VIDEO_EMU" ]] && source $SET_DISPLAY_SH $VIDEO # set display
 
 # Show exit splash
 ${TBASH} show_splash.sh exit
