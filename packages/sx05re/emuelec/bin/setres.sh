34a203166859d19b7e64e6191e64ef43c461687b
Joshua L <Langerz82@users.noreply.github.com>
2022-01-24 05:09:35 +1100

    Update setres.sh

--- packages/sx05re/emuelec/bin/setres.sh
+++ packages/sx05re/emuelec/bin/setres.sh
@@ -81,6 +81,7 @@ case $MODE in
 		echo 0 > /sys/class/graphics/fb1/free_scale
 		;;
 	480cvbs)
+    echo 480cvbs > /sys/class/display/mode
 		fbset -fb /dev/fb0 -g 640 480 640 960 $BPP
 		fbset -fb /dev/fb1 -g $BPP $BPP $BPP $BPP $BPP
 		echo 0 0 639 479 > /sys/class/graphics/fb0/free_scale_axis
@@ -90,6 +91,7 @@ case $MODE in
 		echo 0 > /sys/class/graphics/fb1/free_scale
 		;;
 	576cvbs)
+    echo 576cvbs > /sys/class/display/mode
 		fbset -fb /dev/fb0 -g 720 576 720 1152 $BPP
 		fbset -fb /dev/fb1 -g $BPP $BPP $BPP $BPP $BPP
 		echo 0 0 719 575 > /sys/class/graphics/fb0/free_scale_axis
