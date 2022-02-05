12ba27444e2133515f7364411975b04598835be5
Langerz82 <langley.joshua@gmail.com>
2022-02-05 23:09:42 +1100

    fixes.

--- packages/sx05re/emulators/dolphinSA/scripts/set_dolphin_joy.sh
+++ packages/sx05re/emulators/dolphinSA/scripts/set_dolphin_joy.sh
@@ -18,31 +18,72 @@ EE_DEV="js0"
 GPFILE=""
 GAMEPAD=""
 
+declare -A GC_DOLPHIN_VALUES=(
+[h0.1]="Axis 7-"
+[h0.4]="Axis 7+"
+[h0.8]="Axis 6-"
+[h0.2]="Axis 6+"
+[b0]="Button 0"
+[b1]="Button 1"
+[b2]="Button 2"
+[b3]="Button 3"
+[b4]="Button 4"
+[b5]="Button 5"
+[b6]="Button 6"
+[b7]="Button 7"
+[b8]="Button 8"
+[b9]="Button 9"
+[b10]="Button 10"
+[b11]="Button 11"
+[b12]="Button 12"
+[b13]="Button 13"
+[b14]="Button 14"
+[b15]="Button 15"
+[b16]="Button 16"
+
+)
+
 declare -A GC_DOLPHIN_BUTTONS=(
   [dpleft]="D-Pad/Left"
   [dpright]="D-Pad/Right"
   [dpup]="D-Pad/Up"
   [dpdown]="D-Pad/Down"
-  [x]="Buttons/X"
-  [y]="Buttons/Y"
-  [a]="Buttons/A"
-  [b]="Buttons/B"
-  [leftshoulder]="Triggers/L"
-  [rightshoulder]="Triggers/R"
+  [x]="Buttons/Y"
+  [y]="Buttons/X"
+  [a]="Buttons/B"
+  [b]="Buttons/A"
+  [lefttrigger]="Triggers/L"
+  [righttrigger]="Triggers/R"
   [start]="Buttons/Start"
+  [rightshoulder]="Buttons/Z"
+)
+
+declare -A GC_DOLPHIN_STICKS=(
+  ["leftx,0"]="Main Stick/Left"
+  ["leftx,1"]="Main Stick/Right"
+  ["lefty,0"]="Main Stick/Up"
+  ["lefty,1"]="Main Stick/Down"
+  ["rightx,0"]="C-Stick/Left"
+  ["rightx,1"]="C-Stick/Right"
+  ["righty,0"]="C-Stick/Up"
+  ["righty,1"]="C-Stick/Down"
 )
 
 # Cleans all the inputs for the gamepad with name $GAMEPAD and player $1 
 clean_pad() {
   #echo "Cleaning pad $1 $2" #debug
+  local P_INDEX=${1}
   [[ -f "${CONFIG_TMP}" ]] && rm "${CONFIG_TMP}"
-  START_DELETING=0
+  local START_DELETING=0
+  local GC_REGEX="\[GCPad[1-9]{1}\]"
   while read -r line; do
-    [[ "[GCPad${1}]" == "$line" ]] && START_DELETING=1
+    if [[ "$line" =~ $GC_REGEX && "[GCPad${P_INDEX}]" != "$line" ]]; then
+      START_DELETING=0
+    fi
+    [[ "[GCPad${P_INDEX}]" == "$line" ]] && START_DELETING=1
     if [[ $START_DELETING == 1 ]]; then
-      [[ "$line" == "[GCPad"[0-9]"]" ]] && [[ "[GCPad${1}]" != "$line" ]] && return
       [[ "$line" =~ ^(.*)+Stick\/Modifier(.*)+$ ]] && echo "$line" >> ${CONFIG_TMP}
-      [[ "$line" =~ ^(.)+Stick\/Dead(.*)+$ ]] && echo "$line" >> ${CONFIG_TMP}
+      [[ "$line" =~ ^(.*)+Stick\/Dead(.*)+$ ]] && echo "$line" >> ${CONFIG_TMP}
       sed -i "1 d" "$CONFIG"
     fi
   done < ${CONFIG}
@@ -55,56 +96,53 @@ clean_pad() {
 # $4 = Device Name
 
 set_pad() {
-  DEVICE_GUID=$3
-  JOY_NAME=$4
+  local DEVICE_GUID=$3
+  local JOY_NAME=$4
 
   echo "DEVICE_GUID=$DEVICE_GUID"
   
-  GC_CONFIG=$(cat "$GCDB" | grep "$DEVICE_GUID")
+  local GC_CONFIG=$(cat "$GCDB" | grep "$DEVICE_GUID")
   echo "GC_CONFIG=$GC_CONFIG"
   [[ -z $GC_CONFIG ]] && return
 
-  GC_MAP=$(echo $GC_CONFIG | cut -d',' -f4-)
-  
+  local GC_MAP=$(echo $GC_CONFIG | cut -d',' -f4-)
+  #local JOY_NAME=$(echo $GC_CONFIG | cut -d',' -f2)
   echo "[GCPad$1]" >> ${CONFIG}
-  echo "Device = evdev/0/$JOY_NAME" >> ${CONFIG}
+  local JOY_INDEX=$(( $1 - 1 ))
+  echo "Device = evdev/${JOY_INDEX}/${JOY_NAME}" >> ${CONFIG}
 
   set -f
-  GC_ARRAY=(${GC_MAP//,/ })
+  local GC_ARRAY=(${GC_MAP//,/ })
   for index in "${!GC_ARRAY[@]}"
   do
-      REC=${GC_ARRAY[$index]}
-      BUTTON_INDEX=$(echo $REC | cut -d ":" -f 1)
-      TVAL=$(echo $REC | cut -d ":" -f 2)
-      BUTTON_VAL=${TVAL:1}
-      TMP_BUTTONS=${GC_DOLPHIN_BUTTONS[$BUTTON_INDEX]}
-      if [[ ! -z "$TMP_BUTTONS" ]]; then
-          GC_INDEX=${GC_DOLPHIN_BUTTONS[$BUTTON_INDEX]}
-          [[ ${TVAL:0:1} == "b" ]] && echo "${GC_INDEX} = Button $BUTTON_VAL" >> ${CONFIG_TMP}
+      local REC=${GC_ARRAY[$index]}
+      local BUTTON_INDEX=$(echo $REC | cut -d ":" -f 1)
+      local TVAL=$(echo $REC | cut -d ":" -f 2)
+      local BUTTON_VAL=${TVAL:1}
+      local GC_INDEX="${GC_DOLPHIN_BUTTONS[$BUTTON_INDEX]}"
+      local BTN_TYPE=${TVAL:0:1}
+      local VAL="${GC_DOLPHIN_VALUES[$TVAL]}"
+      
+      # CREATE BUTTON MAPS (inlcuding hats).
+      if [[ ! -z "$GC_INDEX" ]]; then
+        if [[ "$BTN_TYPE" == "b"  || "$BTN_TYPE" == "h" ]]; then
+          [[ ! -z "$VAL" ]] && echo "${GC_INDEX} = ${VAL}" >> ${CONFIG_TMP}
+        fi
       fi
+
+      # Create Axis Maps
       case $BUTTON_INDEX in
-        "lefttrigger")
-          # unused
+        lefttrigger|righttrigger)
+          if [[ "$BTN_TYPE" == "a" ]]; then
+            VAL=${BUTTON_VAL}
+            echo "${GC_INDEX} = Axis $VAL+" >> ${CONFIG_TMP}
+          fi
           ;;
-        "righttrigger")
-          [[ ${TVAL:0:1} == "b" ]] && echo "Buttons/Z = Button $BUTTON_VAL" >> ${CONFIG_TMP}
-          [[ ${TVAL:0:1} == "a" ]] && echo "Buttons/Z = Axis $BUTTON_VAL+" >> ${CONFIG_TMP}
-          ;;
-        "leftx")
-          echo "Main Stick/Left  = Axis $BUTTON_VAL-" >> ${CONFIG_TMP}
-          echo "Main Stick/Right  = Axis $BUTTON_VAL+" >> ${CONFIG_TMP}
-          ;;
-        "lefty")
-          echo "Main Stick/Up  = Axis $BUTTON_VAL-" >> ${CONFIG_TMP}
-          echo "Main Stick/Down  = Axis $BUTTON_VAL+" >> ${CONFIG_TMP}
-          ;;
-        "rightx")
-          echo "C-Stick/Left  = Axis $BUTTON_VAL-" >> ${CONFIG_TMP}
-          echo "C-Stick/Right  = Axis $BUTTON_VAL+" >> ${CONFIG_TMP}
-          ;;
-        "righty")
-          echo "C-Stick/Up  = Axis $BUTTON_VAL-" >> ${CONFIG_TMP}
-          echo "C-Stick/Down  = Axis $BUTTON_VAL+" >> ${CONFIG_TMP}
+        leftx|lefty|rightx|righty)
+          GC_INDEX="${GC_DOLPHIN_STICKS[${BUTTON_INDEX},0]}"
+          echo "$GC_INDEX = Axis $BUTTON_VAL-" >> ${CONFIG_TMP}
+          GC_INDEX="${GC_DOLPHIN_STICKS[${BUTTON_INDEX},1]}"
+          echo "$GC_INDEX = Axis $BUTTON_VAL+" >> ${CONFIG_TMP}
           ;;
       esac
   done
@@ -115,7 +153,7 @@ set_pad() {
   GC_RECORD=$(cat ${CONFIG_TMP} | grep -E "^$JOYSTICK\/Modifier\/Range *\= *(.*)$")
   [[ -z "$GC_RECORD" ]] && echo "$JOYSTICK/Modifier/Range = 50.000000000000000" >> ${CONFIG_TMP}
   GC_RECORD=$(cat ${CONFIG_TMP} | grep -E "^$JOYSTICK\/Dead Zone *\= *(.*)$")
-  [[ -z "$GC_RECORD" ]] && echo "$stick/Dead Zone = 25.000000000000000" >> ${CONFIG_TMP}
+  [[ -z "$GC_RECORD" ]] && echo "$JOYSTICK/Dead Zone = 25.000000000000000" >> ${CONFIG_TMP}
 
   JOYSTICK="C-Stick"
   GC_RECORD=$(cat ${CONFIG_TMP} | grep -E "^$JOYSTICK\/Modifier *\= *(.*)$")
@@ -129,12 +167,11 @@ set_pad() {
   rm "${CONFIG_TMP}"
 }
 
-# This will extract the GUID from es_settings.cfg depending on how many players have been set on ES and determine if they are currently connected to the device.
 get_players() {
 # You can set up to 8 player on ES
   declare -i PLAYER=1
   
-  cat /proc/bus/input/devices | grep -E "^I\: (.*)$|^H\: Handlers\=.*${JSI}.*+$|^N\: Name\=\"(.*)+$|^B\: KEY\=[0-9a-f ]+$" > /tmp/input_devices
+  cat /proc/bus/input/devices | grep -E -B5 -A3 "^H: Handlers\=.*js[0-9]{1}.*+$" > /tmp/input_devices
   
   for ((y = 1; y <= 8; y++)); do
     #echo "Getting GUID for INPUT P${y}GUID" #debug
@@ -149,22 +186,22 @@ get_players() {
     local LINE_NUM=$(cat /tmp/input_devices | grep -E -n "${DETECT_LINE}" | cut -d : -f 1)
     [[ ! $LINE_NUM =~ ^[0-9]+$ ]] && continue
 
-    ((LINE_NUM++))
-    local JOY_KEY=$(cat /tmp/input_devices | sed -n "${LINE_NUM}p" | grep -E "^B\: KEY\=[0-9a-f ]+$")
+    declare -i LINE_KEY_NUM=$(( LINE_NUM+3 ))
+    local JOY_KEY=$(cat /tmp/input_devices | sed -n "${LINE_KEY_NUM}p" | grep -E "^B\: KEY\=[0-9a-f ]+$")
     [[ -z "$JOY_KEY" ]] && continue
 
-    ((LINE_NUM-=2))
-    local JOY_NAME=$(cat /tmp/input_devices | sed -n "${LINE_NUM}p" | cut -d "=" -f 2 | tr -d '"')
+    declare -i LINE_NAME_NUM=$(( LINE_NUM-4 ))
+    local JOY_NAME=$(cat /tmp/input_devices | sed -n "${LINE_NAME_NUM}p" | cut -d "=" -f 2 | tr -d '"')
     [[ -z "$JOY_NAME" ]] && continue
 
-    ((LINE_NUM--))
-    local GUID_LINE=$(cat /tmp/input_devices | sed -n "${LINE_NUM}p")
+    ((LINE_NAME_NUM--))
+    local GUID_LINE=$(cat /tmp/input_devices | sed -n "${LINE_NAME_NUM}p")
 
     DEVICE_GUID=$(generate_guid "$GUID_LINE")
     [[ -z "$DEVICE_GUID" ]] && continue
 
     if [[ ! -z "${DEVICE_GUID}" ]]; then
-        clean_pad
+        clean_pad "${PLAYER}"
   	    set_pad "${PLAYER}" "${JSI}" "${DEVICE_GUID}" "${JOY_NAME}"
   	fi
     ((PLAYER++))
