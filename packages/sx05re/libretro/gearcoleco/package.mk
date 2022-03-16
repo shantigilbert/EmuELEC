d0bee06d5961af590581f29031cb91b0d785f5ba
Kelvfimer <calvarro@gmail.com>
2022-03-16 17:06:35 +0100

    added emulator gearcoleco 443207ea3cf3df75299f3346692b873189c44183

---
+++ packages/sx05re/libretro/gearcoleco/package.mk
@@ -0,0 +1,45 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
+#
+#  This Program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2, or (at your option)
+#  any later version.
+#
+#  This Program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.tv; see the file COPYING.  If not, write to
+#  the Free Software Foundation, 51 Franklin Street, Suite 500, Boston, MA 02110, USA.
+#  http://www.gnu.org/copyleft/gpl.html
+################################################################################
+
+PKG_NAME="gearcoleco"
+PKG_VERSION="443207ea3cf3df75299f3346692b873189c44183"
+PKG_SHA256="97dd3eb67c5388431d6ecfb7b54842d753520a489abb3e3529d349cdc8431d5a"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPLv3"
+PKG_SITE="https://github.com/drhelius/Gearcoleco/"
+PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="libretro"
+PKG_SHORTDESC="Gearcoleco is a very accurate cross-platform ColecoVision emulator written in C++"
+PKG_LONGDESC="Gearcoleco is a very accurate cross-platform ColecoVision emulator written in C++"
+PKG_TOOLCHAIN="make"
+#GET_HANDLER_SUPPORT="git"
+
+make_target() {
+  make -C platforms/libretro/
+}
+
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib/libretro
+  cp platforms/libretro/gearcoleco_libretro.so $INSTALL/usr/lib/libretro/
+}
