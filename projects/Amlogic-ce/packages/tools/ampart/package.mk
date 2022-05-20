986e83a32d767380b37338f4fe3a4150a2e363c9
7Ji <pugokushin@gmail.com>
2022-05-21 01:03:38 +0800

    ampart: add package for modifying emmc partition table

---
+++ projects/Amlogic-ce/packages/tools/ampart/package.mk
@@ -0,0 +1,18 @@
+# SPDX-License-Identifier: GPL-3.0
+# Copyright (C) 2022-present 7Ji (pugokushin@gmail.com)
+
+PKG_NAME="ampart"
+PKG_VERSION="cb576fa0e170303572361a466799f70b3b6c57bd"
+PKG_SHA256="29acb7cefb6929bc30b0470a9b98c2bd2fbfc014410e0d2ce008f2c0fc057222"
+PKG_LICENSE="GPL3"
+PKG_SITE="https://github.com/7Ji/ampart"
+PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
+PKG_MAINTAINER="7Ji"
+PKG_LONGDESC="A simple, fast, yet reliable partition tool for Amlogic's proprietary emmc partition format."
+PKG_DEPENDS_TARGET="toolchain"
+PKG_TOOLCHAIN="make"
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/sbin
+  cp -a $PKG_BUILD/ampart $INSTALL/usr/sbin
+}
