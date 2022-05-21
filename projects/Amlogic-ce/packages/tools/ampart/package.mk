ec9c9f0e316b1a8fe1360a16138e2315f8037d9a
7Ji <pugokushin@gmail.com>
2022-05-21 17:28:06 +0800

    ampart: bump version to 4b970d

--- projects/Amlogic-ce/packages/tools/ampart/package.mk
+++ projects/Amlogic-ce/packages/tools/ampart/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2022-present 7Ji (pugokushin@gmail.com)
 
 PKG_NAME="ampart"
-PKG_VERSION="fc4a0537f92b1bb54ce4af95b66fc0d624f6572b"
-PKG_SHA256="84213abc98c109499bd373822ef3c1d352b35e56f64b4fc55fa2b48375b27571"
+PKG_VERSION="4b970de1869206addc93ea42fc69d41a14c97cc5"
+PKG_SHA256="99d6858bd569c86bc68f961513b22ee2772e9179609518d2a24efedc41c5e70e"
 PKG_LICENSE="GPL3"
 PKG_SITE="https://github.com/7Ji/ampart"
 PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
