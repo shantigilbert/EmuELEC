970ed0924c7bf96c0a0ad39ab1f27d218e3822b6
7Ji <pugokushin@gmail.com>
2022-05-23 00:13:06 +0800

    amaprt: bump version

--- projects/Amlogic-ce/packages/tools/ampart/package.mk
+++ projects/Amlogic-ce/packages/tools/ampart/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2022-present 7Ji (pugokushin@gmail.com)
 
 PKG_NAME="ampart"
-PKG_VERSION="a522c190e8b5ba738b4d1bd46b9f04f0982c26eb"
-PKG_SHA256="a38e53804cd4243abd3b3de8ac445c023c3821815dcb316113dbafc3bb7e04f4"
+PKG_VERSION="78cc3e0a136e81b6f10b46c2e00929c24a403c96"
+PKG_SHA256="5c9f145ed2cbe77bd34e5c4b0415d5cba43c1e1850be9cae5a8adc33829eafbd "
 PKG_LICENSE="GPL3"
 PKG_SITE="https://github.com/7Ji/ampart"
 PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
