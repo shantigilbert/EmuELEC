582aa42ef42b0947c742d858c0eceeb39362e923
Joshua L <Langerz82@users.noreply.github.com>
2022-07-14 09:46:39 +1000

    rebase

--- packages/sx05re/emuelec/bin/setres.sh
+++ packages/sx05re/emuelec/bin/setres.sh
@@ -2,6 +2,7 @@
 
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
+# Copyright (C) 2022-present Joshua L (https://github.com/Langerz82)
 
 # Read the video output mode and set it for emuelec to avoid video flicking.
 
@@ -9,17 +10,6 @@
 # Allowed argument example ./setres.sh 1080p60hz <-- For height 1080 pixels.
 
 # set -x #echo on
-
-# 1080p60hz
-# 1080i60hz
-# 720p60hz
-# 720p50hz
-# 480p60hz
-# 480cvbs
-# 576p50hz
-# 1080p50hz
-# 1080i50hz
-# 576cvbs
 
 # arg1, 1 = Hides, 0 = Show.
 show_buffer ()
@@ -35,37 +25,31 @@ blank_buffer()
 }
 
 BPP=32
-HZ=60
-
 MODE=$1
-CUR_MODE=`cat /sys/class/display/mode`;
 
 # If the current display is the same as the change just exit.
 [ -z "$MODE" ] && exit 0;
 [[ $MODE == "auto" ]] && exit 0;
-# Removed because if try to set invalid video mode it becomes a valid MODE
-# in display/mode and then when trying to revert back this becomes true exiting.
-#[[ "$MODE" == "$CUR_MODE" ]] && exit 0;
 
 if [[ ! "$MODE" == *"x"* ]]; then
   case $MODE in
-  	*p*) H=$(echo $MODE | cut -d'p' -f 1) ;;
-  	*i*) H=$(echo $MODE | cut -d'i' -f 1) ;;
-  	*cvbs*) H=$(echo $MODE | cut -d'c' -f 1) ;;
+    *p*) H=$(echo $MODE | cut -f'p' -f 1) ;;
+    *i*) H=$(echo $MODE | cut -f'i' -f 1) ;;
   esac
-fi
-
-HZ=${MODE:(-4):2}
-if [[ ! -n "$HZ" ]] || [[ $HZ -eq 50 ]]; then
-	HZ=60
 fi
 
 
 # hides buffer
 show_buffer 1
 
+if [[ "$EE_PROJECT" == "Rockchip" ]]; then
+  echo $MODE > /sys/class/display/display0.HDMI/mode  
+else
+  echo $MODE > /sys/class/display/mode
+fi
+
 case $MODE in
-	480p*hz|480i*hz|576p*hz|720p*hz|1080p*hz|1440p*hz|2160p*hz|576i*hz|720i*hz|1080i*hz|1440i*hz|2160i*hz)
+	480p*|480i*|576p*|720p*|1080p*|1440p*|2160p*|576i*|720i*|1080i*|1440i*|2160i*)
     W=$(($H*16/9))
     [[ "$MODE" == "480"* ]] && W=854
 		DH=$(($H*2))
@@ -73,7 +57,6 @@ case $MODE in
 		H1=$(($H-1))
 		fbset -fb /dev/fb0 -g $W $H $W $DH $BPP
 		fbset -fb /dev/fb1 -g $BPP $BPP $BPP $BPP $BPP
-		echo $MODE > /sys/class/display/mode
 		echo 0 > /sys/class/graphics/fb0/free_scale
 		echo 1 > /sys/class/graphics/fb0/freescale_mode
 		echo 0 0 $W1 $H1 > /sys/class/graphics/fb0/free_scale_axis
@@ -81,7 +64,6 @@ case $MODE in
 		echo 0 > /sys/class/graphics/fb1/free_scale
 		;;
 	480cvbs)
-    echo 480cvbs > /sys/class/display/mode
 		fbset -fb /dev/fb0 -g 640 480 640 960 $BPP
 		fbset -fb /dev/fb1 -g $BPP $BPP $BPP $BPP $BPP
 		echo 0 0 639 479 > /sys/class/graphics/fb0/free_scale_axis
@@ -91,7 +73,6 @@ case $MODE in
 		echo 0 > /sys/class/graphics/fb1/free_scale
 		;;
 	576cvbs)
-    echo 576cvbs > /sys/class/display/mode
 		fbset -fb /dev/fb0 -g 720 576 720 1152 $BPP
 		fbset -fb /dev/fb1 -g $BPP $BPP $BPP $BPP $BPP
 		echo 0 0 719 575 > /sys/class/graphics/fb0/free_scale_axis
@@ -110,8 +91,6 @@ case $MODE in
   		H1=$(($H-1))
   		fbset -fb /dev/fb0 -g $W $H $W $DH $BPP
   		fbset -fb /dev/fb1 -g $BPP $BPP $BPP $BPP $BPP
-#  		[[ "$MODE" = "720x480p"* ]] && MODE=$(echo "${H}p${HZ}hz")
-  		echo $MODE > /sys/class/display/mode
   		echo 0 > /sys/class/graphics/fb0/free_scale
   		echo 1 > /sys/class/graphics/fb0/freescale_mode
   		echo 0 0 $W1 $H1 > /sys/class/graphics/fb0/free_scale_axis
