30097d496cd2b0e8d76ecc2445aada595bb1e0a1
7Ji <pugokushin@gmail.com>
2022-07-20 00:16:39 +0800

    (lib32-|)mali-bifrost: clean SONAME

--- packages/lib32/graphics/lib32-mali-bifrost/package.mk
+++ packages/lib32/graphics/lib32-mali-bifrost/package.mk
@@ -37,6 +37,10 @@ makeinstall_target() {
   BLOB=${PKG_BUILD}/${BLOB}
   cp ${BLOB} ${INSTALL}/usr/lib32/libmali.so
   cp ${BLOB} ${SYSROOT_PREFIX}/usr/lib/libmali.so
+  # The mali blob comes with a hardcoded libmali.so.1 SONAME, it confuses ldconfig and libGLESv2.so, libGLESv3.so, etc won't be indexed as a result
+  # It is always a BAD idea to code in a SONAME that is not what the library is actually used as, we need to clean it so it can be treated as 
+  # libGLES.so, libgbm.so, etc and ldconfig can caches it in ld.so.cache
+  patchelf --set-soname "" ${INSTALL}/usr/lib32/libmali.so
 
   local LINK_LIST="libEGL.so \
                    libEGL.so.1 \
