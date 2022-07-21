d2431d8bccb188060b01ce00393708eda03935d2
7Ji <pugokushin@gmail.com>
2022-07-22 00:15:19 +0800

    lib32-retrorun: depend on linux:host

--- packages/lib32/emuelec/libretro/lib32-retrorun/package.mk
+++ packages/lib32/emuelec/libretro/lib32-retrorun/package.mk
@@ -8,7 +8,7 @@ PKG_ARCH="aarch64"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/navy1978/retrorun-go2"
 PKG_URL=""
-PKG_DEPENDS_TARGET="lib32-toolchain lib32-libgo2 lib32-libdrm"
+PKG_DEPENDS_TARGET="lib32-toolchain lib32-libgo2 lib32-libdrm linux:host"
 PKG_PATCH_DIRS+=" $(get_pkg_directory retrorun)/patches"
 PKG_TOOLCHAIN="make"
 PKG_BUILD_FLAGS="lib32"
@@ -24,8 +24,8 @@ unpack() {
 pre_configure_target() {
   local TARGET_KERNEL_PATCH_ARCH=aarch64
   CFLAGS+=" -I$(get_build_dir lib32-libdrm)/include/drm"
-  CFLAGS+=" -I$(get_build_dir lib32-linux-headers)/include/uapi"
-  CFLAGS+=" -I$(get_build_dir lib32-linux-headers)/tools/include"
+  CFLAGS+=" -I$(get_build_dir linux)/include/uapi"
+  CFLAGS+=" -I$(get_build_dir linux)/tools/include"
   sed -i "s|/storage/.config/distribution/|/emuelec/|g" ${PKG_BUILD}/src/main.cpp
   safe_remove ${PKG_BUILD}/retrorun
 }
