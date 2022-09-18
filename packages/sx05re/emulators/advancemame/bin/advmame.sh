c6554f348579d48b7597aba5cf1c59490bbaf93d
Langerz82 <langley.joshua@gmail.com>
2022-09-18 13:14:54 +1000

    intial commit.

--- packages/sx05re/emulators/advancemame/bin/advmame.sh
+++ packages/sx05re/emulators/advancemame/bin/advmame.sh
@@ -33,11 +33,16 @@ if [ "$EE_DEVICE" != "OdroidGoAdvance" ] && [ "$EE_DEVICE" != "GameForce" ]; the
     unset DISPLAY
     MODE=`cat /sys/class/display/mode`;
     sed -i '/device_video_modeline/d' $CONFIG_DIR/advmame.rc
+ sed -i '/sync_fps/d' $CONFIG_DIR/advmame.rc
+ sed -i '/device_video_clock/d' $CONFIG_DIR/advmame.rc
+ sed -i '/sync_speed/d' $CONFIG_DIR/advmame.rc
+ echo "sync_speed 1.0" >> $CONFIG_DIR/advmame.rc
 
     if [[ -f "/ee_s905" && "$MODE" == "1080p"* ]]; then
         MODE="720p60hz"
     fi
 
+    FPS="60"
     case "$MODE" in
         "480p"*)
             echo "device_video_modeline 720x480 15.246 720 762 834 968 480 484 491 525 +hsync +vsync" >> $CONFIG_DIR/advmame.rc
@@ -48,16 +53,21 @@ if [ "$EE_DEVICE" != "OdroidGoAdvance" ] && [ "$EE_DEVICE" != "GameForce" ]; the
         "1080p"*)
             echo "device_video_modeline 1920x1080_60.00 153.234 1920 1968 2121 2168 1080 1127 1130 1178 +hsync +vsync" >> $CONFIG_DIR/advmame.rc
         ;;
-        "1280x1024p60hz"*)
+        "1280x1024p60hz")
             echo "device_video_modeline 1280x1024_60.00 108.88 1280 1360 1496 1712 1024 1025 1028 1060 +hsync +vsync" >> $CONFIG_DIR/advmame.rc
         ;;
-        "1024x768p60hz"*)
+        "1024x768p60hz")
             echo "device_video_modeline 1024x768_60.00 64.11 1024 1080 1184 1344 768 769 772 795 +hsync +vsync" >> $CONFIG_DIR/advmame.rc
         ;;
-        "640x480p60hz"*)
+        "800x600p60hz")
+            echo "device_video_modeline 800x600_60.00 38.22 800 832 912 1024 600 601 604 622 +hsync +vsync" >> $CONFIG_DIR/advmame.rc
+        ;;
+        "640x480p60hz")
             echo "device_video_modeline 640x480_60.00 23.86 640 656 720 800 480 481 484 497 +hsync +vsync" >> $CONFIG_DIR/advmame.rc
         ;;
     esac
+    echo "sync_fps ${FPS}" >> $CONFIG_DIR/advmame.rc
+    echo "device_video_clock 1-200 / 1-200 / ${FPS}" >> $CONFIG_DIR/advmame.rc
 fi
 
 ROMNAME=$(basename $1)
