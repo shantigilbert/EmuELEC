4184276ef463256dff0ab138b1ff394dd21be537
Kelvfimer <calvarro@gmail.com>
2022-10-19 23:28:44 +0200

    xash3d latest update

--- packages/sx05re/emuelec-ports/xash3d/package.mk
+++ packages/sx05re/emuelec-ports/xash3d/package.mk
@@ -3,7 +3,7 @@
 # Copyright (C) 2022-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="xash3d"
-PKG_VERSION="360dc4f7ed664e1c10beb799fe6070d6a5bc36c4"
+PKG_VERSION="305b2579ebf36c91770925c3fc510294899da931"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/FWGS/xash3d-fwgs"
 PKG_URL="https://github.com/FWGS/xash3d-fwgs.git"
