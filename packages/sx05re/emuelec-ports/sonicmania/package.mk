b50d5c11af0a5a3eb0d11b7012347d52bfbc854d
Kelvfimer <calvarro@gmail.com>
2022-10-19 23:28:22 +0200

    Sonicmania latest update

--- packages/sx05re/emuelec-ports/sonicmania/package.mk
+++ packages/sx05re/emuelec-ports/sonicmania/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2022-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="sonicmania"
-PKG_VERSION="ff1183befba3d58dd8c5b3ab79b712f86c01d826"
+PKG_VERSION="09626c79e72124de42edc260343a4cca2af334a2"
 PKG_ARCH="any"
 PKG_SITE="https://github.com/Rubberduckycooly/Sonic-Mania-Decompilation"
 PKG_URL="$PKG_SITE.git"
