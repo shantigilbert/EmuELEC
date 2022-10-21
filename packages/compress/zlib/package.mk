053694a4c66ca2c129654c916003c1e0fe32b7ba
Shin8K <60099362+Shin8K@users.noreply.github.com>
2022-10-22 00:25:44 +0800

    update zlib to 1.2.13

--- packages/compress/zlib/package.mk
+++ packages/compress/zlib/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="zlib"
-PKG_VERSION="1.2.12"
+PKG_VERSION="1.2.13"
 PKG_SHA256="d14c38e313afc35a9a8760dadf26042f51ea0f5d154b0630a31da0540107fb98"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.zlib.net"
