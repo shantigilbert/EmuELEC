7e7ea9ac3998a3842f7f2754a5d4b13f0dad135c
jurafish <jurafish@163.com>
2022-10-27 00:32:55 +0800

    Update package.mk

--- packages/compress/zlib/package.mk
+++ packages/compress/zlib/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="zlib"
-PKG_VERSION="1.2.12"
+PKG_VERSION="1.2.13"
 PKG_SHA256="7db46b8d7726232a621befaab4a1c870f00a90805511c0e0090441dac57def18"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.zlib.net"
