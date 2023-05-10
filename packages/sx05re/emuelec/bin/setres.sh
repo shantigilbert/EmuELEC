4abe4b3792e1c1aa477b19692348d1551005aea4
Joshua L <Langerz82@users.noreply.github.com>
2023-05-10 11:29:20 +1000

    Update setres.sh
    Fixes to main fbset call in setres.sh

--- packages/sx05re/emuelec/bin/setres.sh
+++ packages/sx05re/emuelec/bin/setres.sh
@@ -85,9 +85,9 @@ set_main_framebuffer() {
   if [[ -n "$SW" && "$SW" > 0 && -n "$SH" && "$SH" > 0 ]]; then
     MSH=$(( SH*2 ))
     if [[ ${EE_DEVICE} == "Amlogic" ]]; then
-      fbset -fb /dev/fb0 -g $FBW $FBH 1920 2160 $BPP
+      fbset -fb /dev/fb0 -g $SW $SH 1920 2160 $BPP
     else
-      fbset -fb /dev/fb0 -g $FBW $FBH $FBW $MFBH $BPP
+      fbset -fb /dev/fb0 -g $SW $SH $SW $MSH $BPP
     fi
     echo 0 0 $(( SW-1 )) $(( SH-1 )) > /sys/class/graphics/fb0/free_scale_axis
     echo 0 > /sys/class/graphics/fb0/free_scale
