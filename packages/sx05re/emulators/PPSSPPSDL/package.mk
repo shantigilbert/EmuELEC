72a3c0d7bb0bb516ecee8cdb261a5fba97d2f0ba
kelvfimer <calvarro@gmail.com>
2023-07-03 21:28:19 +0200

    Latest PPSSPPSDL with Ra

--- packages/sx05re/emulators/PPSSPPSDL/package.mk
+++ packages/sx05re/emulators/PPSSPPSDL/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="PPSSPPSDL"
-PKG_VERSION="6315fae2dc7e0b8245ea34eee551c7bc955cca40"
+PKG_VERSION="5017c1520e47e4a4d8dcca040c639c577b2338b4"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="MAME"
