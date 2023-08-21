deae0c379e4323f92a9e426f724e8d17ef3c456e
kelvfimer <calvarro@gmail.com>
2023-08-21 12:24:22 +0200

    bump box64 7a992837a13eecbcccb54c93c77ee8e1a9c74d1c

--- packages/sx05re/emulators/box64/package.mk
+++ packages/sx05re/emulators/box64/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2021-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="box64"
-PKG_VERSION="bc8227d2bda7124c354bb9b618c5feea0dc60db7"
+PKG_VERSION="7a992837a13eecbcccb54c93c77ee8e1a9c74d1c"
 
 # amlogic-old uses older linux headers not compatible with newer versions
 [ "${DEVICE}" == "Amlogic-old" ] && PKG_VERSION="6392550208eadf07419692920acc2955bb844af7"
