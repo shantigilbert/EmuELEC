cbca7bcd5ba4ca56245e4d0581cae7485635f702
kelvfimer <calvarro@gmail.com>
2023-09-09 15:51:11 +0200

    Add ports Fallout 1 and Fallout 2

---
+++ packages/sx05re/emuelec-ports/falloutce2/package.mk
@@ -0,0 +1,21 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2020-present Shanti Gilbert (https://github.com/shantigilbert)
+
+PKG_NAME="falloutce2"
+PKG_VERSION="91806d8ccb4215c9df3bf626112d4f101fba6e6b"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_SITE="https://github.com/alexbatalov/fallout2-ce"
+PKG_URL="$PKG_SITE.git"
+PKG_DEPENDS_TARGET="toolchain SDL2"
+PKG_LONGDESC="Game port of Fallout 2 using SDL2"
+PKG_TOOLCHAIN="cmake"
+GET_HANDLER_SUPPORT="git"
+
+makeinstall_target() {
+mkdir -p $INSTALL/usr/bin
+cp $PKG_BUILD/.${TARGET_NAME}/fallout2-ce $INSTALL/usr/bin
+cp -rf $PKG_DIR/scripts/* $INSTALL/usr/bin
+mkdir -p $INSTALL/usr/config/emuelec/configs/falloutce2
+cp $PKG_DIR/config/* $INSTALL/usr/config/emuelec/configs/falloutce2
+}
