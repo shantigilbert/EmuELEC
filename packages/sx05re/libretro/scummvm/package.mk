734becbb04bdb0c9e4a55c111d10713eb1ec5dbf
kelvfimer <calvarro@gmail.com>
2023-09-24 14:57:04 +0200

    Scummvm libretro update to a56c2b83153c84b0a1c0f3f9a18c38f23b430fc9

--- packages/sx05re/libretro/scummvm/package.mk
+++ packages/sx05re/libretro/scummvm/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="scummvm"
-PKG_VERSION="0c3e3aaebcaf23de7175a9df5e5c0e13ad2551fd"
+PKG_VERSION="a56c2b83153c84b0a1c0f3f9a18c38f23b430fc9"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/scummvm"
