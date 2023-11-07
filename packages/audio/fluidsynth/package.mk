a49101311f39cede11610778a33734d8cdd10582
kelvfimer <calvarro@gmail.com>
2023-11-07 23:45:52 +0100

    update fluidsyth

--- packages/audio/fluidsynth/package.mk
+++ packages/audio/fluidsynth/package.mk
@@ -12,8 +12,7 @@ PKG_DEPENDS_TARGET="toolchain glib libsndfile"
 PKG_LONGDESC="FluidSynth renders midi music files as raw audio data, for playing or conversion."
 PKG_BUILD_FLAGS="+pic"
 
-PKG_CMAKE_OPTS_TARGET="-DBUILD_SHARED_LIBS=0 \
-                       -DLIB_SUFFIX= \
+PKG_CMAKE_OPTS_TARGET="-DLIB_SUFFIX= \
                        -Denable-libsndfile=1 \
                        -Denable-pkgconfig=1 \
                        -Denable-pulseaudio=0 \
