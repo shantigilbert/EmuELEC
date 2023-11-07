6ad1dd6540f89d5f81a367e805010e08a1f2f0d3
kelvfimer <calvarro@gmail.com>
2023-11-07 23:52:36 +0100

    SDL2_mixer latest version

--- packages/sx05re/tools/SDL2/SDL2_mixer/package.mk
+++ packages/sx05re/tools/SDL2/SDL2_mixer/package.mk
@@ -2,22 +2,33 @@
 # Copyright (C) 2019-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="SDL2_mixer"
-PKG_VERSION="2.0.4"
+PKG_VERSION="2.6.3"
 PKG_SHA256=""
 PKG_LICENSE="GPLv3"
-PKG_SITE="http://www.libsdl.org/projects/SDL_mixer/release"
 PKG_URL="$PKG_SITE/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain alsa-lib SDL2 mpg123-compat libvorbis libvorbisidec libogg opusfile libmodplug flac"
-PKG_LONGDESC="SDL_mixer 2.0.1"
+PKG_URL="https://github.com/libsdl-org/SDL_mixer/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain alsa-lib SDL2 mpg123-compat libvorbis libvorbisidec libogg opusfile libmodplug flac fluidsynth"
+PKG_LONGDESC="SDL2 mixer"
 PKG_DEPENDS_HOST="toolchain:host SDL2:host"
 
-pre_configure_target() {
+pre_configure_host() {
+  PKG_CMAKE_OPTS_HOST="-DSDL2MIXER_OPUS=OFF \
+                       -DSDL2MIXER_MOD=OFF \
+                       -DSDL2MIXER_MP3=OFF \
+                       -DSDL2MIXER_FLAC=OFF \
+                       -DSDL2MIXER_MIDI=OFF \
+                       -DSDL2MIXER_VORBIS=OFF \
+                       -DSDL2MIXER_OGG=OFF"
+}
 
+pre_configure_target() {
+  SDL2_CONFIG=${SYSROOT_PREFIX}/usr/bin/sdl2-config
   PKG_CONFIGURE_OPTS_TARGET="--disable-sdltest \
-							 --disable-music-midi-fluidsynth \
-							 --enable-music-flac \
-							 --enable-music-mod-modplug \
-							 --enable-music-ogg-tremor \
-							 --enable-music-ogg \
-							 --enable-music-mp3"
+			     --disable-music-midi-fluidsynth \
+			     --enable-music-flac \
+			     --enable-music-mod-modplug \
+			     --enable-music-ogg-tremor \
+			     --enable-music-ogg \
+			     --enable-music-mp3"
+#  PKG_CMAKE_OPTS_TARGET="-DSDL2MIXER_OPUS=OFF"
 }
