9e5bf7c064f3216a08b8d29db7535af00ae29ef4
kelvfimer <calvarro@gmail.com>
2023-11-10 13:47:49 +0100

    SDL2_mixer CMAKE values

--- packages/sx05re/tools/SDL2/SDL2_mixer/package.mk
+++ packages/sx05re/tools/SDL2/SDL2_mixer/package.mk
@@ -5,8 +5,8 @@ PKG_NAME="SDL2_mixer"
 PKG_VERSION="2.6.3"
 PKG_SHA256=""
 PKG_LICENSE="GPLv3"
+PKG_SITE="http://www.libsdl.org/projects/SDL_mixer/release"
 PKG_URL="$PKG_SITE/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_URL="https://github.com/libsdl-org/SDL_mixer/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain alsa-lib SDL2 mpg123-compat libvorbis libvorbisidec libogg opusfile libmodplug flac fluidsynth"
 PKG_LONGDESC="SDL2 mixer"
 PKG_DEPENDS_HOST="toolchain:host SDL2:host"
@@ -23,12 +23,14 @@ pre_configure_host() {
 
 pre_configure_target() {
   SDL2_CONFIG=${SYSROOT_PREFIX}/usr/bin/sdl2-config
-  PKG_CONFIGURE_OPTS_TARGET="--disable-sdltest \
-			     --disable-music-midi-fluidsynth \
-			     --enable-music-flac \
-			     --enable-music-mod-modplug \
-			     --enable-music-ogg-tremor \
-			     --enable-music-ogg \
-			     --enable-music-mp3"
-#  PKG_CMAKE_OPTS_TARGET="-DSDL2MIXER_OPUS=OFF"
+  PKG_CMAKE_OPTS_TARGET="-DSDL2MIXER_MIDI_FLUIDSYNTH=OFF \
+                       -DSDL2MIXER_FLAC=ON \
+                       -DSDL2MIXER_MOD_MODPLUG=ON \
+                       -DSDL2MIXER_VORBIS_TREMOR=ON \
+                       -DSDL2MIXER_OGG=ON \
+                       -DSDL2MIXER_MP3=ON \
+                       -DSDL2MIXER_SAMPLES=OFF \
+                       -DSDL2MIXER_MOD_MODPLUG_SHARED=OFF \
+                       -DSDL2MIXER_MOD_XMP=OFF \
+                       -DSDL2MIXER_WAVPACK=OFF"
 }
