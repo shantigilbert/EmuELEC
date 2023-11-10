66c1b74eb1d36a96ee01ecbf033fa20006d916a7
kelvfimer <calvarro@gmail.com>
2023-11-10 16:11:32 +0100

    swanstation update to latest version

--- packages/sx05re/emulators/swanstation/package.mk
+++ packages/sx05re/emulators/swanstation/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2020-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="swanstation"
-PKG_VERSION="376744746a6880b5eec7ac48b5c006c9ae8c6770"
-PKG_SHA256="b269f8b9954cd266619d3cc1f1af779a9c7a114b494c9709dfaa4b9ba3f76b18"
+PKG_VERSION="afb6bc73c7ce522190b19ec3e3c8a14eb264bdeb"
+PKG_SHA256="318004c8285baa9195023d4ca39d4914e93b3c5ec10fc1092e460625cbeb6212"
 PKG_ARCH="aarch64"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/libretro/swanstation"
