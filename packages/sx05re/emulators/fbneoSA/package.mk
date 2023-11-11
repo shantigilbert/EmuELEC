fbea6c775e275dac96749b97b9b1f1048f17922d
kelvfimer <calvarro@gmail.com>
2023-11-11 22:25:11 +0100

    latest fbneoSA commit

--- packages/sx05re/emulators/fbneoSA/package.mk
+++ packages/sx05re/emulators/fbneoSA/package.mk
@@ -2,15 +2,7 @@
 # Copyright (C) 2022-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="fbneoSA"
-#PKG_VERSION="f16edfe129258ff22cc6a8b9dd536ebc47f759d6"
-#PKG_VERSION="59e8f7f4bfb0993009e7e1e1d62e7af7000f61cb"
-#PKG_VERSION="24f77006fffb402e7af42c0176598b62d6f4826e"
-#PKG_VERSION="742a7aa167c9bb42c26183ae68501739be79e082"
-#PKG_VERSION="bb0fc3427cc33335b8ec492259fc8e8a03d4a729"
-#PKG_VERSION="d80b805393f6d2fb72b8ef85c76e8f0fbba8b864"
-#PKG_VERSION="ce1ba20b6e64679b6f49b279d0e74ee3d8f77109"
-#PKG_VERSION="f179eb0cf4050ea5d37ff65efb5efe56d5108be5"
-PKG_VERSION="4fd53a0c6cb9e007f392795100281c73c7018a7f"
+PKG_VERSION="3e70c4d01818d81db2452222cb535abacf13ddd1"
 PKG_ARCH="aarch64"
 PKG_LICENSE="Custom"
 PKG_SITE="https://github.com/finalburnneo/FBNeo"
@@ -20,7 +12,7 @@ PKG_LONGDESC="https://github.com/finalburnneo/FBNeo/blob/master/src/license.txt"
 GET_HANDLER_SUPPORT="git"
 PKG_TOOLCHAIN="make"
 
-PKG_MAKE_OPTS_TARGET=" sdl2 RELEASEBUILD=1 FORCE_SYSTEM_LIBPNG=1"
+PKG_MAKE_OPTS_TARGET="sdl2 RELEASEBUILD=1"
 
 pre_configure_target() {
 sed -i "s|\`sdl2-config|\`${SYSROOT_PREFIX}/usr/bin/sdl2-config|g" makefile.sdl2
