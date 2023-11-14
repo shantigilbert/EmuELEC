9f05b250f39e8144489ebeb6023bc8ed55fc907e
kelvfimer <calvarro@gmail.com>
2023-11-14 22:34:53 +0100

    Soniccd Latest update

--- packages/sx05re/emuelec-ports/soniccd/package.mk
+++ packages/sx05re/emuelec-ports/soniccd/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2021-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="soniccd"
-PKG_VERSION="8b08b44f141f9e66873951b63bb441ac9bbfc62d"
+PKG_VERSION="aea302ed5dc4a5bf82bad04594c51e9be3ed6f59"
 PKG_ARCH="any"
 PKG_SITE="https://github.com/Rubberduckycooly/Sonic-CD-11-Decompilation"
 PKG_URL="$PKG_SITE.git"
