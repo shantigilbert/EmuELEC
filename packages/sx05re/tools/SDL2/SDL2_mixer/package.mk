18aa3e1c313a9a0bbeef73efe411019d4193b9c8
kelvfimer <calvarro@gmail.com>
2023-11-14 21:54:16 +0100

    Updated SDL2_mixer 32 bit and 64

--- packages/sx05re/tools/SDL2/SDL2_mixer/package.mk
+++ packages/sx05re/tools/SDL2/SDL2_mixer/package.mk
@@ -2,22 +2,36 @@
 # Copyright (C) 2019-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="SDL2_mixer"
-PKG_VERSION="2.0.4"
+PKG_VERSION="2.6.3"
 PKG_SHA256=""
 PKG_LICENSE="GPLv3"
 PKG_SITE="http://www.libsdl.org/projects/SDL_mixer/release"
 PKG_URL="$PKG_SITE/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain alsa-lib SDL2 mpg123-compat libvorbis libvorbisidec libogg opusfile libmodplug flac"
-PKG_LONGDESC="SDL_mixer 2.0.1"
+PKG_DEPENDS_TARGET="toolchain alsa-lib SDL2 mpg123-compat libvorbis libvorbisidec libogg opusfile libmodplug flac fluidsynth"
+PKG_LONGDESC="SDL2 mixer"
 PKG_DEPENDS_HOST="toolchain:host SDL2:host"
 
-pre_configure_target() {
-
-  PKG_CONFIGURE_OPTS_TARGET="--disable-sdltest \
-							 --disable-music-midi-fluidsynth \
-							 --enable-music-flac \
-							 --enable-music-mod-modplug \
-							 --enable-music-ogg-tremor \
-							 --enable-music-ogg \
-							 --enable-music-mp3"
+pre_configure_host() {
+  PKG_CMAKE_OPTS_HOST="-DSDL2MIXER_OPUS=OFF \
+                       -DSDL2MIXER_MOD=OFF \
+                       -DSDL2MIXER_MP3=OFF \
+                       -DSDL2MIXER_FLAC=OFF \
+                       -DSDL2MIXER_MIDI=OFF \
+                       -DSDL2MIXER_VORBIS=OFF \
+                       -DSDL2MIXER_OGG=OFF"
 }
+
+pre_configure_target() {
+  SDL2_CONFIG=${SYSROOT_PREFIX}/usr/bin/sdl2-config
+  PKG_CMAKE_OPTS_TARGET="-DSDL2MIXER_MIDI_FLUIDSYNTH=OFF \
+                       -DSDL2MIXER_FLAC=ON \
+                       -DSDL2MIXER_MOD_MODPLUG=ON \
+                       -DSDL2MIXER_VORBIS_TREMOR=ON \
+                       -DSDL2MIXER_OGG=ON \
+                       -DSDL2MIXER_MP3=ON \
+                       -DSDL2MIXER_SAMPLES=OFF \
+                       -DSDL2MIXER_MOD_MODPLUG_SHARED=OFF \
+                       -DSDL2MIXER_MOD_XMP=OFF \
+                       -DSDL2MIXER_WAVPACK=OFF"
+}
+
