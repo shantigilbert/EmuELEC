8199d8c3b00809aeac4c5f616333ae4936514d09
kelvfimer <calvarro@gmail.com>
2024-02-03 23:07:30 +0100

    fixing a typo

--- packages/sx05re/emuelec/bin/emuelecRunEmu.sh
+++ packages/sx05re/emuelec/bin/emuelecRunEmu.sh
@@ -159,12 +159,6 @@ case ${PLATFORM} in
             RUNTHIS='${TBASH} stella.sh "${ROMNAME}"'
 		fi
 		;;
-	"fmtmarty")
-		if [ "$EMU" = "fmtowns" ]; then
-            set_kill_keys "fmtowns"
-            RUNTHIS='${TBASH} fmtowns.sh"'
-		fi
-		;;
 	"atarist")
 		if [ "$EMU" = "HATARISA" ]; then
             set_kill_keys "hatari"
