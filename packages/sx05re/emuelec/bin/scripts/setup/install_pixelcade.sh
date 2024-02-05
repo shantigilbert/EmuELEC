73d42a7b84d1c7173446635449c71c21befc8f38
shantigilbert <shantic@gmail.com>
2024-02-05 13:52:36 -0600

    update Java JDK to zulu21.32.17-ca-jdk21.0.2
    install_java is now available from profile for any script that needs it
    Updated install_pixelcade.sh and freej2me.sh

--- packages/sx05re/emuelec/bin/scripts/setup/install_pixelcade.sh
+++ packages/sx05re/emuelec/bin/scripts/setup/install_pixelcade.sh
@@ -235,53 +235,8 @@ if [[ -d "${INSTALLPATH}pixelcade" ]]; then
     fi
 fi
 
-#. /etc/profile
-
-#ee_console enable
-#echo "Checking JDK..." > /dev/console
-
-JDKDEST="${HOME}/roms/bios/jdk"
-JDKNAME="zulu18.0.45-ea-jdk18.0.0-ea.18"
-CDN="https://cdn.azul.com/zulu/bin"
-
-# Alternate just for reference
-#CDN="https://cdn.azul.com/zulu-embedded/bin"
-
-mkdir -p "${JDKDEST}"
-
-OLDVERSION="$(cat ${JDKDEST}/eeversion 2>/dev/null)"
-if [ "${JDKNAME}" != "${OLDVERSION}" ]; then
-   JDKINSTALLED="no"
-   rm -rf "${JDKDEST}"
-   mkdir -p "${JDKDEST}"
-fi
-
-if [ "${JDKINSTALLED}" == "no" ]; then
-echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80
-if [ $? -ne 0 ]; then
-    echo "No internet connection, exiting..."
-    text_viewer -e -w -t "No Internet!" -m "You need to be connected to the internet to download the JDK\nNo internet connection, exiting...";
-    exit 1
-fi
-    echo "Downloading JDK please be patient..."
-    cd ${JDKDEST}/..
-    wget "${CDN}/${JDKNAME}-linux_aarch64.tar.gz"
-    echo "Inflating JDK please be patient..."
-    tar xvfz ${JDKNAME}-linux_aarch64.tar.gz ${JDKNAME}-linux_aarch64/lib
-    tar xvfz ${JDKNAME}-linux_aarch64.tar.gz ${JDKNAME}-linux_aarch64/bin
-    tar xvfz ${JDKNAME}-linux_aarch64.tar.gz ${JDKNAME}-linux_aarch64/conf
-    rm ${JDKNAME}-linux_aarch64/lib/*.zip
-    mv ${JDKNAME}-linux_aarch64/* ${JDKDEST}
-    rm -rf ${JDKNAME}-linux_aarch64*
-
-    for del in jmods include demo legal man DISCLAIMER LICENSE readme.txt release Welcome.html; do
-        rm -rf ${JDKDEST}/${del}
-    done
-    echo "JDK done! loading core!"
-    cp -rf /usr/lib/libretro/freej2me-lr.jar ${HOME}/roms/bios
-    echo "${JDKNAME}" > "${JDKDEST}/eeversion"
-fi
-
+# Lets check if java is installed and up to date, called from profile
+install_java;
 
 if [ ${PIXELCADE_PRESENT} == "false" ]; then  #skip this if the user already had pixelcade
   echo "Installing Pixelcade from GitHub Repo..."
