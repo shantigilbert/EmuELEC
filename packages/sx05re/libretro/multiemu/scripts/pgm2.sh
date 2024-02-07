c4e9030b24acdcfb6a7da994515fd02a37242895
kelvfimer <calvarro@gmail.com>
2024-02-07 22:03:52 +0100

    scripts changed

--- packages/sx05re/libretro/multiemu/scripts/pgm2.sh
+++ packages/sx05re/libretro/multiemu/scripts/pgm2.sh
@@ -4,6 +4,8 @@
 # Copyright (C) 2021-present Shanti Gilbert (https://github.com/shantigilbert)
 
 if [ ! -f "/storage/roms/bios/mame/ini/mame.ini" ]; then
+    if [ ! -d "/storage/roms/bios/mame/ini" ]; then
     mkdir /storage/roms/bios/mame/ini
+    fi
     cp -rf "/usr/config/emuelec/configs/multiemu/ini/"mame* "/storage/roms/bios/mame/ini"
 fi
