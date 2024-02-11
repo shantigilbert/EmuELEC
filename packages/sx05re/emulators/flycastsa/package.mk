e53cb57d3917734d6524637b0e85c8bb03939bdf
kelvfimer <calvarro@gmail.com>
2024-02-11 04:37:47 +0100

    flycast latest

--- packages/sx05re/emulators/flycastsa/package.mk
+++ packages/sx05re/emulators/flycastsa/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2021-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="flycastsa"
-PKG_VERSION="7029e1615a215bc43e51f8eac605f31dd01ba8cd"
+PKG_VERSION="44fa364f36c43bed19b055096600f075c656f78c"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/flyinghead/flycast"
 PKG_URL="${PKG_SITE}.git"
