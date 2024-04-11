413319ee4ff90f963a0e9b5723125df652772825
kelvfimer <calvarro@gmail.com>
2024-04-11 09:32:11 +0200

    Half Life latest version

--- packages/sx05re/emuelec-ports/xash3d/package.mk
+++ packages/sx05re/emuelec-ports/xash3d/package.mk
@@ -3,7 +3,7 @@
 # Copyright (C) 2022-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="xash3d"
-PKG_VERSION="05ff0d6020860b970dcf9b60f4d4abebc620c2da"
+PKG_VERSION="1677835b45d8c3bae3a53fda2838de4c38a2da0f"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/FWGS/xash3d-fwgs"
 PKG_URL="https://github.com/FWGS/xash3d-fwgs.git"
