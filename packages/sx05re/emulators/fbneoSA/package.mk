991312cba6644729756cd5769169cf76924953c5
kelvfimer <calvarro@gmail.com>
2024-06-02 07:29:57 +0200

    fbneoSA latest update

--- packages/sx05re/emulators/fbneoSA/package.mk
+++ packages/sx05re/emulators/fbneoSA/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2022-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="fbneoSA"
-PKG_VERSION="5e472792e6aed55270f728bd5a3bf3576d6b6019"
+PKG_VERSION="c122e875b8e74e731476ec62ca33866ff4c23a51"
 PKG_ARCH="aarch64"
 PKG_LICENSE="Custom"
 PKG_SITE="https://github.com/finalburnneo/FBNeo"
