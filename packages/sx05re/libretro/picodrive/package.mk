a183716848a8c73f44b533adc94b63d19c06a0ef
kelvfimer <calvarro@gmail.com>
2024-06-02 07:06:10 +0200

    yabasanshiro update latest

--- packages/sx05re/libretro/picodrive/package.mk
+++ packages/sx05re/libretro/picodrive/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="picodrive"
-PKG_VERSION="db2b6d996608f7323b04dcf425bee2796a36dbbe"
+PKG_VERSION="695e6de85e06d47043003664531fc491eaa91240"
 PKG_LICENSE="MAME"
 PKG_SITE="https://github.com/irixxxx/picodrive"
 PKG_URL="${PKG_SITE}.git"
