477a5e07f80fd1e7e16d3af9b1d40554e7a92ab1
kelvfimer <calvarro@gmail.com>
2024-06-14 11:51:12 +0200

    fbneo latest update

--- packages/sx05re/libretro/fbneo/package.mk
+++ packages/sx05re/libretro/fbneo/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="fbneo"
-PKG_VERSION="4e0738a16d64adb4c468db4144ec3b51f9217660"
-PKG_SHA256="4d39ac5922a82deb4c86d01c5cddd06e332c6449d09bf484e099d282b7e01039"
+PKG_VERSION="634d207adade213bbe83684be2a183d158f56e52"
+PKG_SHA256="2b89fa899145589acbfc0ba26e47687773868b4680fd08b3c57a694f37a753cf"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="Non-commercial"
