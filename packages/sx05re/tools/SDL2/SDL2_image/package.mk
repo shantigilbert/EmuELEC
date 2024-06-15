fe6554d905bba446c7b21723ff2d5280a50372e0
kelvfimer <calvarro@gmail.com>
2024-06-15 06:42:46 +0200

    SDL2_image update using https://github.com/ROCKNIX/distribution/commit/a1f551eef49edefd138a08f0443139e594dcfc21 makes hypseus latest version work. Thx @spycat88 for making this PR

--- packages/sx05re/tools/SDL2/SDL2_image/package.mk
+++ packages/sx05re/tools/SDL2/SDL2_image/package.mk
@@ -3,11 +3,25 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="SDL2_image"
-PKG_VERSION="2.6.3"
-PKG_SHA256="931c9be5bf1d7c8fae9b7dc157828b7eee874e23c7f24b44ba7eff6b4836312c"
+PKG_VERSION="2.8.2"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.libsdl.org/"
-PKG_URL="https://www.libsdl.org/projects/SDL_image/release/SDL2_image-${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain SDL2"
+PKG_URL="https://github.com/libsdl-org/SDL_image/releases/download/release-${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain SDL2 libjpeg-turbo libpng"
 PKG_LONGDESC="SDL_image is an image file loading library. "
+PKG_TOOLCHAIN="configure"
 
+PKG_CONFIGURE_OPTS_TARGET="--disable-sdltest \
+                           --enable-bmp \
+                           --enable-gif \
+                           --enable-jpg \
+                           --enable-lbm \
+                           --enable-pcx \
+                           --enable-png \
+                           --enable-pnm \
+                           --enable-tga \
+                           --enable-tif \
+                           --enable-xcf \
+                           --enable-xpm \
+                           --enable-xv \
+                           --enable-webp"
