7c2d7f743a6169ba396a5de666abf5e94b718c22
Joshua L <Langerz82@users.noreply.github.com>
2024-07-17 02:32:35 +1000

    Dev port nativevideo - Allows ports to use native video option from ES. (#1371)
    * initial commit.
    
    * simplify runapp cmd
    
    * Update emuelecRunApp.sh
    
    removed uneeded variables in runapp.

---
+++ packages/sx05re/emuelec/bin/emuelecRunApp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
+# Copyright (C) 2024-present Langerz82 (https://github.com/Langerz82)
+
+# Source predefined functions and variables
+. /etc/profile
+
+arguments="$@"
+
+# Extract the platform name from the arguments
+PLATFORM="${arguments##*-P}"  # read from -P onwards
+PLATFORM="${PLATFORM%% *}"  # until a space is found
+
+ROMNAME="${1}"
+
+emuelec-utils init_app_video "${PLATFORM}" "${ROMNAME}"
+
+"$@"
+
+emuelec-utils end_app_video
