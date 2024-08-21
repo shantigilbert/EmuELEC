5143ae895af85a22c269c222bb7717923cc52a30
Joshua L <Langerz82@users.noreply.github.com>
2024-08-22 05:21:30 +1000

    Update ppsspp.sh (#1373)
    get_ee_setting not found because profile file not included.

--- packages/sx05re/emulators/PPSSPPSDL/scripts/ppsspp.sh
+++ packages/sx05re/emulators/PPSSPPSDL/scripts/ppsspp.sh
@@ -3,6 +3,8 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
+. /etc/profile
+
 AUTOGP=$(get_ee_setting ppsspp_auto_gamepad)
 if [[ "${AUTOGP}" == "1" ]]; then
 	set_ppsspp_joy.sh
