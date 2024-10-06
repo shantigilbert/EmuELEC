c2a2bd7c01245f4e04d28a7216947fe6582fa5f6
kelvfimer <calvarro@gmail.com>
2024-10-06 20:13:00 +0200

    Duckstation cheevos

--- packages/sx05re/emulators/duckstation/scripts/duckstation.sh
+++ packages/sx05re/emulators/duckstation/scripts/duckstation.sh
@@ -31,13 +31,8 @@ if [[ "${AUTOGP}" == "1" ]]; then
 	set_duckstation_joy.sh
 fi
 
-#Setting Cheevos if enabled on emulationstation. First check if token entry is not present
-ztoken=$(grep "Token =" /storage/.config/emuelec/configs/duckstation/settings.ini)
+duckstationcheevos.sh
 
-if ([ -z "${ztoken}" ]) 
-then
-    duckstationcheevos.sh
-fi
 
 if [[ "${1}" == *"duckstation_gui.pbp"* ]]; then
     duckstation-nogui -fullscreen
