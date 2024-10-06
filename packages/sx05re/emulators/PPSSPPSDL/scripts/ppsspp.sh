6b0f539926df1e856f5da46a367508ff6ee07cd1
kelvfimer <calvarro@gmail.com>
2024-10-06 20:06:04 +0200

    PPSSPP Cheevos

--- packages/sx05re/emulators/PPSSPPSDL/scripts/ppsspp.sh
+++ packages/sx05re/emulators/PPSSPPSDL/scripts/ppsspp.sh
@@ -3,12 +3,12 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
-. /etc/profile
-
 AUTOGP=$(get_ee_setting ppsspp_auto_gamepad)
 if [[ "${AUTOGP}" == "1" ]]; then
 	set_ppsspp_joy.sh
 fi
+
+ppssppcheevos.sh
 
 ARG=${1//[\\]/}
 export SDL_AUDIODRIVER=alsa          
